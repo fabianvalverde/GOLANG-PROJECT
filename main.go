974193d99674b42fb03ed7251d3292123e81998f
Fabián Valverde <110683318+fabianvalverde@users.noreply.github.com>
2023-05-14 14:53:11 -0600

    hjkvk

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Homepage Endpoint Hit")
+}
+
+func handleRequest(){
+	http.HandleFunc("/", homePage)
+	log.Fatal()
+}
