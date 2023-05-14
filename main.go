0f72c83a1b2aa678aa2acc91c61afbccc90eb18e
Fabián Valverde <110683318+fabianvalverde@users.noreply.github.com>
2023-05-14 17:11:29 -0600

    xcdg

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
+type User struct {
+	User_Id          int    `json:"user_id"`
+	Name             string `json:"name"`
+	Email            string `json:"email"`
+	Password         string `json:"password"`
+	Shipping_address string `json:"shipping_address"`
+}
+
+type Users []User
+
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	users := Users{
+		User{User_Id: 123, Name: "Fabian", Email: "fabian3830@gmail.com", Password: "1245", Shipping_address: "fdgdfgd"},
+	}
+
+	fmt.Println("Endpoint Hit: All users Endpoint")
+	json.NewEncoder(w).Encode(users)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
 }
 
-func handleRequest(){
+func handleRequest() {
 	http.HandleFunc("/", homePage)
-	log.Fatal()
+	http.HandleFunc("/users", allUsers)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequest()
 }
